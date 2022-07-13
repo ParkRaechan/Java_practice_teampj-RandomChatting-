e6083d78565045ec2692d2c604e3e5bacc8c6402
ParkRaechan <focks1012@naver.com>
2022-07-13 17:18:21 +0900

    Initial commit

---
+++ src/main/java/RandomChatting/controller/IndexController.java
@@ -0,0 +1,13 @@
+package RandomChatting.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//홈-매핑
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
